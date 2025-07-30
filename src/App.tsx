cffb2a85f4c4b6baf6a23fd2c820114b5d75cb6e
Justin-SJ-Hong <madwolves98@gmail.com>
2025-07-30 20:57:31 +0900

    question: 고민하고 있는 내용을 App.tsx에 주석 형식으로 적음

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import emailjs from '@emailjs/browser';
 
+/* 
+  고민하고 있는 것
+  1. 계약 제안서 보기 페이지에서 수락 버튼을 누를 경우 서명 진행 후 자동으로 계약을 생성
+  2. 자동으로 계약을 생성하면 생성한 계약을 데이터베이스 및 PDF 파일로 저장한 다음 제안자와 수락자 모두가 받을 수 있게 하기
+  3. 계약 제안서 보기 페이지에서 거절 버튼을 누를 경우 거절 사유를 입력해서 상대방에게 이메일로 보내고, 해당 계약 제안서 상태를 거절로 변경
+*/
 
 function App() {
   const [isAuthChecked, setIsAuthChecked] = useState(false);
